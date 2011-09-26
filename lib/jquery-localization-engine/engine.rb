fab04dad572deabfc83e9aa4622650798d7983d2
Raphael Randschau <nicolai86@me.com>
2011-09-26 22:05:11 +0200

    try to remove double init

--- lib/jquery-localization-engine/engine.rb
+++ lib/jquery-localization-engine/engine.rb
@@ -1,8 +1,4 @@
 module JqueryLocalizationEngine
   class Engine < Rails::Engine
-    initialize "jquery-localization.app" do |app|
-      JqueryLocalizationEngine.setup do |config|
-      end
-    end
   end
 end
