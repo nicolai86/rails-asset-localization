9710fa23d49cfcc50e9c04e07bfe1788162c6a59
Raphael Randschau <nicolai86@me.com>
2011-10-06 13:23:05 +0200

    update README, change locales_controller scope

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ In your Gemfile:
 
 That's it! Your locales are now available under `/locales/:locale`
 
+## What it does
+
+Your locales will be served as in JSON, and converted to define a Handlebars compatible binding.
+E.g. `Welcome %{user}!` will be converted to `Welcome {{user}}!`
+
+If you are using the haml\_asset gem you'll have access to a modified version of `I18n.t` which renders your localizations using the `I18n.default_locale`, and wraps the results into a span so you can later on use jQuery-localize to dynamically re-localize the entire page!
+
 ## Todo
 
   - Specs
--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,11 +1,9 @@
-module JqueryLocalizationEngine
-  class LocalesController < ::ApplicationController
-    def locale
-      logger.debug "jquery-localize for #{params[:locale]}"
-      locale = params[:locale].to_sym
-      respond_to do |format|
-        format.js { render json: I18n.backend.send(:translations)[locale].to_json }
-      end
+class LocalesController < ::ApplicationController
+  def locale
+    logger.debug "jquery-localize for #{params[:locale]}"
+    locale = params[:locale].to_sym
+    respond_to do |format|
+      format.js { render json: I18n.backend.send(:translations)[locale].to_json }
     end
   end
 end
