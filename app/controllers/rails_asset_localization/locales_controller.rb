1b60b27d99b8bba9573b7c4e8b89f9c57ae379b3
Raphael Randschau <nicolai86@me.com>
2013-03-28 22:19:01 +0100

    move locales controller into namespace

---
+++ app/controllers/rails_asset_localization/locales_controller.rb
@@ -0,0 +1,14 @@
+module RailsAssetLocalization
+  class LocalesController < ::ApplicationController
+    respond_to :json
+
+    def locale
+      locale = params.fetch(:locale) { I18n.default_locale.to_s }
+      locale = $1 if locale =~ /(\w+)\-(\w+)/
+
+      I18n.backend.send(:init_translations)
+
+      respond_with I18n.backend.send(:translations)[locale.to_sym].to_json
+    end
+  end
+end
