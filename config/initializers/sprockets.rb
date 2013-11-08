d729a449655ef64fe7e10a6de3d17bb249f093d3
Raphael Randschau <nicolai86@me.com>
2013-11-08 20:01:19 +0100

    use Rails.root.join instead of String interpolation

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -1,6 +1,6 @@
 class Sprockets::DirectiveProcessor
   def process_depend_on_locales_directive
-    Dir.glob("#{Rails.root}/config/locales/**.yml").each do |path|
+    Dir.glob(Rails.root.join("config","locales", "**.yml").to_s).each do |path|
       context.depend_on(path)
     end
   end
