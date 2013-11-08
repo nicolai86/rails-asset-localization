03c481bea14693760a4581958ad750cf9880441e
Raphael Randschau <nicolai86@me.com>
2013-11-08 20:06:18 +0100

    make simple test case pass again

--- test/rails-localization-engine_test.rb
+++ test/rails-localization-engine_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class RailsLocalizationEngineTest < ActiveSupport::TestCase
   test "truth" do
-    assert_kind_of Module, RailsLocalizationEngine
+    assert_kind_of Module, RailsAssetLocalization
   end
 end
