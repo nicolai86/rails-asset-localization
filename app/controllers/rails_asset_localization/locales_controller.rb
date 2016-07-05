0e29413617da376e45ea8143948d37da352848ad
Lucas Yang <lucasmf@yahoo-inc.com>
2016-07-05 15:06:32 +0800

    make localesExporter a static variable to avoid instantiating per request

--- app/controllers/rails_asset_localization/locales_controller.rb
+++ app/controllers/rails_asset_localization/locales_controller.rb
@@ -5,10 +5,13 @@ module RailsAssetLocalization
     def locale
       locale = params.fetch(:locale) { I18n.default_locale.to_s }
       locale = $1 if locale =~ /(\w+)\-(\w+)/
-
-      locales_exporter = ::RailsAssetLocalization::LocalesExporter.new
       locales = (locales_exporter.translations[locale.to_sym] || {})
       respond_with locales
+    end
+
+    def locales_exporter
+      @@locales_exporter ||= ::RailsAssetLocalization::LocalesExporter.new
+      @@locales_exporter
     end
   end
 end
